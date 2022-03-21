3e6d5e4ec3eeeeff31c93f458b47b15de6bdafba
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-03-21 20:43:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+
+from classes.Root import Root
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Root, '/')
+
+if __name__ == '__main__':
+    app.run()
