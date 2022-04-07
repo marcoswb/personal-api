27ddd4c8ce42ae5a554d48f11ee26bc7bf20276d
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-04-07 20:34:19 -0300

    create 'contacts' route

--- controllers/Contacts.py
+++ controllers/Contacts.py
@@ -1,5 +1,16 @@
+from flask import jsonify
 from flask_restful import Resource
+from models.General import General
 
 class Contacts(Resource):
     def get(self):
-        return 'metodo get contacts'
+        database = General()
+        database.connect()
+        result = database.select(
+            General.linkedin_link,
+            General.github_link,
+            General.email,
+            General.number_phone
+        ).dicts()
+
+        return jsonify(result[0])
---
+++ models/General.py
@@ -0,0 +1,30 @@
+import peewee
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+db = peewee.PostgresqlDatabase(
+    getenv('DB_DATABASE'),
+    user=getenv('DB_USER'),
+    password=getenv('DB_PASSWORD'),
+    host=getenv('DB_HOST')
+)
+
+class General(peewee.Model):
+    name = peewee.CharField()
+    full_name = peewee.CharField()
+    occupation = peewee.CharField()
+    about = peewee.TextField()
+    email = peewee.CharField()
+    number_phone = peewee.CharField()
+    github_link = peewee.CharField()
+    linkedin_link = peewee.CharField()
+
+    class Meta:
+        database = db 
+
+    def connect(self):
+        try:
+            General.create_table()
+        except peewee.OperationalError:
+            pass
