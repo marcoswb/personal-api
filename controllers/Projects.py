ca2da7c74c721e178d8c93610c079da6bf190982
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-04-19 20:53:30 -0300

    implemented project route

--- controllers/Projects.py
+++ controllers/Projects.py
@@ -1,5 +1,23 @@
+from flask import jsonify
 from flask_restful import Resource
+
+from models.Projects import Projects as ModelProjects
 
 class Projects(Resource):
     def get(self):
-        return 'metodo get projects'
+        database = ModelProjects()
+        database.connect()
+        result = database.select(
+            ModelProjects.name,
+            ModelProjects.description,
+            ModelProjects.techs
+        ).dicts()
+
+        if result:
+            projects = []
+            for project in result:
+                projects.append(project)
+                
+            return jsonify(projects)
+        else:
+            return jsonify([])
