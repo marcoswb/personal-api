8acce3825ec923b735bda461f0d32ed14ca44fe1
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-04-19 21:02:56 -0300

    implemented blog route

--- controllers/Blog.py
+++ controllers/Blog.py
@@ -1,5 +1,24 @@
+from flask import jsonify
 from flask_restful import Resource
+
+from models.Blog import Blog as ModelBlog
 
 class Blog(Resource):
     def get(self):
-        return 'metodo get blog'
+        database = ModelBlog()
+        database.connect()
+        result = database.select(
+            ModelBlog.title,
+            ModelBlog.description,
+            ModelBlog.tags,
+            ModelBlog.link
+        ).dicts()
+
+        if result:
+            blog_posts = []
+            for posts in result:
+                blog_posts.append(posts)
+                
+            return jsonify(blog_posts)
+        else:
+            return jsonify([])
---
+++ models/Blog.py
@@ -0,0 +1,26 @@
+import peewee
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+db = peewee.PostgresqlDatabase(
+    getenv('DB_DATABASE'),
+    user=getenv('DB_USER'),
+    password=getenv('DB_PASSWORD'),
+    host=getenv('DB_HOST')
+)
+
+class Blog(peewee.Model):
+    title = peewee.CharField()
+    description = peewee.CharField()
+    tags = peewee.CharField()
+    link = peewee.CharField()
+
+    class Meta:
+        database = db 
+
+    def connect(self):
+        try:
+            Blog.create_table()
+        except peewee.OperationalError:
+            pass
