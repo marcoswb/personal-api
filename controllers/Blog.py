c09bdd737df190ef7bfd5505e05dca023139a7b9
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-04-20 20:45:40 -0300

    adjustment blog and projects route

--- controllers/Blog.py
+++ controllers/Blog.py
@@ -1,24 +1,8 @@
 from flask import jsonify
 from flask_restful import Resource
 
-from models.Blog import Blog as ModelBlog
-
 class Blog(Resource):
     def get(self):
-        database = ModelBlog()
-        database.connect()
-        result = database.select(
-            ModelBlog.title,
-            ModelBlog.description,
-            ModelBlog.tags,
-            ModelBlog.link
-        ).dicts()
-
-        if result:
-            blog_posts = []
-            for posts in result:
-                blog_posts.append(posts)
-                
-            return jsonify(blog_posts)
-        else:
-            return jsonify([])
+        blog_posts = []
+        
+        return jsonify(blog_posts)
--- controllers/Projects.py
+++ controllers/Projects.py
@@ -1,24 +1,8 @@
 from flask import jsonify
 from flask_restful import Resource
 
-from models.Projects import Projects as ModelProjects
-
 class Projects(Resource):
     def get(self):
-        database = ModelProjects()
-        database.connect()
-        result = database.select(
-            ModelProjects.name,
-            ModelProjects.description,
-            ModelProjects.techs,
-            ModelProjects.link
-        ).dicts()
-
-        if result:
-            projects = []
-            for project in result:
-                projects.append(project)
-                
-            return jsonify(projects)
-        else:
-            return jsonify([])
+        projects = []
+            
+        return jsonify(projects)
