a81b0bc8cf24121d72e5b855ab5727a43056d932
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-06-15 11:03:41 -0300

    get posts from db

--- controllers/Blog.py
+++ controllers/Blog.py
@@ -1,35 +1,22 @@
 from flask import jsonify
 from flask_restful import Resource
-import requests
-from xml.etree import ElementTree
-from dotenv import load_dotenv
-from os import getenv
+from models.Blog import Blog as BlogSQLite
 
 class Blog(Resource):
-    load_dotenv()
-    medium_user = getenv('MEDIUM_USER')
 
     def get(self):
-        response = requests.get(f'https://medium.com/feed//@{self.medium_user}')
-                    
+        sqlite = BlogSQLite()
+        sqlite.connect()
+        data = sqlite.select().dicts()
+
         blog_posts = []
-        posts = ElementTree.fromstring(response.content)[0]
-        for post in posts.findall('item'):
-
-            categories = []
-            for category in post.findall('category'):
-                categories.append(category.text)
-
-            description = ''
-            for elem in post.iter():
-                if 'encoded' in elem.tag:
-                    position_first_paragraph = (elem.text.find('<p>')+25)
-                    description = elem.text[position_first_paragraph:position_first_paragraph+50]
+        for post in data:
+            categories = post['categories'].split(',')
 
             blog_posts.append({
-                'name': post.find('title').text,
-                'description': f'{description.capitalize()}...',
-                'link': post.find('link').text,
+                'name': post['name'],
+                'description': post['description'],
+                'link': post['link'],
                 'categories': categories
             })
         
