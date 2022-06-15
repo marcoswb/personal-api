690fc4a3ecf98555bfd33b4f6465768ce532372c
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-06-15 11:03:59 -0300

    remove env variables

--- controllers/Projects.py
+++ controllers/Projects.py
@@ -1,13 +1,9 @@
 from flask import jsonify
 from flask_restful import Resource
-from dotenv import load_dotenv
-from os import getenv
 
 from models.Projects import Project
 
 class Projects(Resource):
-    load_dotenv()
-    github_user = getenv('GITHUB_USER')
 
     def get(self):
         try:
@@ -17,13 +13,13 @@ class Projects(Resource):
             projects = []
             data = sqlite.select().dicts()
             for project in data:
-                    languages = project['languages'].split(',')
-                    projects.append({
-                        'name': project['name'],
-                        'description': project['description'],
-                        'link': project['link'],
-                        'languages': languages
-                    })
+                languages = project['languages'].split(',')
+                projects.append({
+                    'name': project['name'],
+                    'description': project['description'],
+                    'link': project['link'],
+                    'languages': languages
+                })
                 
             return jsonify(projects)
         except:
