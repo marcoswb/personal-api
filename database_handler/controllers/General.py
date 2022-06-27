34d0c2d3dc9496bd5b771f294f129f8d8ec3fba6
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-06-27 20:30:46 -0300

    get general tab from database

--- database_handler/controllers/General.py
+++ database_handler/controllers/General.py
@@ -1,7 +1,31 @@
+from models.General import General
+
 class Controller:
 
     def get_data(self):
-        data = [('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste')]
+        database = General()
+        database.connect()
+        result = database.select(
+            General.name,
+            General.short_description,
+            General.number_phone,
+            General.about,
+            General.linkedin_link,
+            General.github_link,
+            General.email,
+            General.full_name
+        ).dicts()
+        
+        data = [(
+            result[0]['name'],
+            result[0]['full_name'],
+            result[0]['short_description'],
+            result[0]['about'],
+            result[0]['email'],
+            result[0]['number_phone'],
+            result[0]['github_link'],
+            result[0]['linkedin_link']
+        )]
         return data
     
     def save_data(self, data):
