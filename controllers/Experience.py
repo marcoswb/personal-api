8b5e93457cac4de8fba2e6cebcdb476c93584ad5
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-06-28 19:58:06 -0300

    send experience to api

--- controllers/Experience.py
+++ controllers/Experience.py
@@ -1,5 +1,7 @@
-from flask import jsonify
+from flask import request, Response, jsonify
 from flask_restful import Resource
+from dotenv import load_dotenv
+from os import getenv
 
 from models.Experience import Experience as ModelExperience
 
@@ -21,3 +23,14 @@ class Experience(Resource):
             return jsonify(experiences)
         else:
             return jsonify([])
+
+    def post(self):
+        load_dotenv()
+
+        token = request.headers['Authorization']
+        expected_token = getenv('TOKEN')
+        if token == expected_token:
+            args = request.json
+            print(args)
+        else:
+            return Response("{'status': 'Unauthorized'}", status=401, mimetype='application/json')
--- database_handler/controllers/Experience.py
+++ database_handler/controllers/Experience.py
@@ -1,6 +1,13 @@
+import requests
+from dotenv import load_dotenv
+from os import getenv
+
 from models.Experience import Experience
 
 class Controller:
+
+    def __init__(self):
+        load_dotenv()
 
     def get_data(self):
         database = Experience()
@@ -18,4 +25,16 @@ class Controller:
         return data
     
     def save_data(self, data):
-        print(data)
+        try:
+            headers = {
+                'Authorization': getenv('TOKEN')
+            }
+            response = requests.post(getenv('ENDPOINT_API'), json=data[0], headers=headers)
+            if response.status_code == 200:
+                return True, ''
+            elif response.status_code == 401:
+                return False, 'Unauthorized'
+            
+            return False, ''
+        except:
+            return False, ''
